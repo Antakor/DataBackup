23140bbab1d7a0a9720359bbde404e86dbe24957
Michael Arndt <ma97@outlook.de>
2015-12-12 17:01:31 +0100

    Added part of backup mechanism

--- DataBackup.py
+++ DataBackup.py
@@ -1,7 +1,8 @@
-from datetime import date
+import datetime
 import os
 import platform
 from copy import deepcopy
+import shutil
 
 
 class Database():
@@ -56,28 +57,58 @@ class Database():
     def getcontent(self):
         return self.__Content
 
-    def addcontent(self, __date, __source, __dest):
+    def addcontent(self, __date, __src, __dst):
         __date = str(__date)
         __date = __date.split("-")
-        __toadd = [__date, __source, __dest]
+        __toadd = [__date, __src, __dst]
         self.__Content.append(__toadd)
 
-    def sourceinlist(self, __source):
+    def srcinlist(self, __src):
         __position = []
         for element in range(0, len(self.__Content)):
-            if self.__Content[element][1] == __source:
+            if self.__Content[element][1] == __src:
                 __position.append(element)
         return __position
 
-    def removecontent(self, __source):
-        __position = self.sourceinlist(__source)
+    def removecontent(self, __src):
+        __position = self.srcinlist(__src)
         if not __position:
             pass
         else:
             for element in range(0, len(__position)):
                 del self.__Content[int(__position[-1])]
                 del __position[-1]
-        del __position
 
+
+class backup():
+    def __init__(self):
+        self.__today = datetime.date.today()
+        self.__filestobackup = self.auto()
+
+    def auto(self):
+        __filestobackup = []
+        __filestocheck = DB.getcontent()
+        for element in range(0, len(__filestocheck)):
+            __yearofelement = int(__filestocheck[element][0][0])
+            __monthofelement = int(__filestocheck[element][0][1])
+            __dayofelement = int(__filestocheck[element][0][2])
+            __datelastbackup = datetime.date.today()
+            __datelastbackup = __datelastbackup.replace(year=__yearofelement, month=__monthofelement, day=__dayofelement)
+            __delta = self.__today - __datelastbackup
+            if __delta.days >= 7:
+                __filestobackup.append(__filestocheck[element])
+        return __filestobackup
+
+    def dobackup(self):
+        while len(self.__filestobackup) != 0:
+            __src = self.__filestobackup[0][1]
+            if os.path.isdir(__src):
+                __dst = self.__filestobackup[0][2]
+                shutil.copytree(__src, __dst, symlinks=False, ignore=None, ignore_dangling_symlinks=True)
+                del self.__filestobackup[0]
+            elif os.path.isfile(__src):
+                __dst = self.__filestobackup[0][2]
+                shutil.copy(__src, __dst)
 
 DB = Database()
+BU = backup()
