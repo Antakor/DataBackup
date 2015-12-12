3a53b1fc188da744f59421a5d5fd813cec38d127
Michael Arndt <ma97@outlook.de>
2015-12-12 13:31:38 +0100

    Complete Database Functions

--- DataBackup.py
+++ DataBackup.py
@@ -1,40 +1,60 @@
 from datetime import date
 import os
 import platform
+from copy import deepcopy
 
 
 class Database():
     def __init__(self):
         if platform.system() == "Windows":
             self.location = str(os.path.dirname(os.path.realpath(__file__)) + "\\backupdatabase.txt")
-        elif platform.system() == "Linux)":
+        elif platform.system() == "Linux":
             self.location = str(os.path.dirname(os.path.realpath(__file__)) + "/backupdatabase.txt")
         else:
-            print("You are using an unsupported operating system. The Programm will end")
+            print("You are using an unsupported operating system. The Programm will stop now.")
             quit()
         if os.path.isfile(self.location) == True:
             pass
         else:
             self.createfile()
+        __filecontent = self.readfile()
+        __filecontent = __filecontent.split("\n")
+        del __filecontent[-1]
+        for element in range(0, len(__filecontent)):
+            __filecontent[element] = __filecontent[element].split("\t")
+        for element in range(0, len(__filecontent)):
+            __filecontent[element][0] = __filecontent[element][0].split("-")
+        self._Content = __filecontent
 
     def createfile(self):
-        _DB = open(self.location, mode="w")
-        _DB.close()
+        __file = open(self.location, mode="w")
+        __file.close()
 
-    def read(self):
-        _DB = open(self.location, mode="r")
-        _DBcontent = _DB.read()
-        _DB.close()
-        return _DBcontent
+    def readfile(self):
+        __file = open(self.location, mode="r")
+        __fileContent = __file.read()
+        __file.close()
+        return __fileContent
 
-    def save(self, _DBcontent):
-        _DB = open(self.location, mode="w")
-        _DB.write(str(_DBcontent))
-        _DB.close()
+    def save(self):
+        __tosave = deepcopy(self._Content)
+        for element in range(0, len(__tosave)):
+            __tosave[element][0] = str(__tosave[element][0][0] + "-" + __tosave[element][0][1] + "-" +__tosave[element][0][2])
+        for element in range(0, len(__tosave)):
+            __tosave[element] = str(__tosave[element][0] + "\t" +__tosave[element][1] + "\t" + __tosave[element][2] + "\n")
+        __tosave = "".join(__tosave)
+        __DB = open(self.location, mode="w")
+        __DB.write(__tosave)
+        __DB.close()
+
+    def getContent(self):
+        return self._Content
+
+    def addContent(self, __date, __source, __dest):
+        __date = str(__date)
+        __date = __date.split("-")
+        __toadd = [__date, __source, __dest]
+        self._Content.append(__toadd)
 
 
 DB = Database()
-lastbackup = str(date.today())
-Content = str(lastbackup + "\n" + lastbackup + "\n")
-DB.save(Content)
-print(DB.read())
