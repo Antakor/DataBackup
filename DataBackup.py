998acfc7c464521b7c8b1881e739e879d25e2e47
Michael Arndt <ma97@outlook.de>
2015-12-12 14:58:34 +0100

    Complete Database Functions #2

--- DataBackup.py
+++ DataBackup.py
@@ -7,16 +7,17 @@ from copy import deepcopy
 class Database():
     def __init__(self):
         if platform.system() == "Windows":
-            self.location = str(os.path.dirname(os.path.realpath(__file__)) + "\\backupdatabase.txt")
+            self.__location = str(os.path.dirname(os.path.realpath(__file__)) + "\\backupdatabase.txt")
         elif platform.system() == "Linux":
-            self.location = str(os.path.dirname(os.path.realpath(__file__)) + "/backupdatabase.txt")
+            self.__location = str(os.path.dirname(os.path.realpath(__file__)) + "/backupdatabase.txt")
         else:
             print("You are using an unsupported operating system. The Programm will stop now.")
             quit()
-        if os.path.isfile(self.location) == True:
+        if os.path.isfile(self.__location) == True:
             pass
         else:
             self.createfile()
+
         __filecontent = self.readfile()
         __filecontent = __filecontent.split("\n")
         del __filecontent[-1]
@@ -24,37 +25,53 @@ class Database():
             __filecontent[element] = __filecontent[element].split("\t")
         for element in range(0, len(__filecontent)):
             __filecontent[element][0] = __filecontent[element][0].split("-")
-        self._Content = __filecontent
+        self.__Content = __filecontent
 
     def createfile(self):
-        __file = open(self.location, mode="w")
+        __file = open(self.__location, mode="w")
         __file.close()
 
     def readfile(self):
-        __file = open(self.location, mode="r")
+        __file = open(self.__location, mode="r")
         __fileContent = __file.read()
         __file.close()
         return __fileContent
 
     def save(self):
-        __tosave = deepcopy(self._Content)
+        __tosave = deepcopy(self.__Content)
         for element in range(0, len(__tosave)):
             __tosave[element][0] = str(__tosave[element][0][0] + "-" + __tosave[element][0][1] + "-" +__tosave[element][0][2])
         for element in range(0, len(__tosave)):
             __tosave[element] = str(__tosave[element][0] + "\t" +__tosave[element][1] + "\t" + __tosave[element][2] + "\n")
         __tosave = "".join(__tosave)
-        __DB = open(self.location, mode="w")
+        __DB = open(self.__location, mode="w")
         __DB.write(__tosave)
         __DB.close()
 
-    def getContent(self):
-        return self._Content
+    def getcontent(self):
+        return self.__Content
 
-    def addContent(self, __date, __source, __dest):
+    def addcontent(self, __date, __source, __dest):
         __date = str(__date)
         __date = __date.split("-")
         __toadd = [__date, __source, __dest]
         self._Content.append(__toadd)
+
+    def sourceinlist(self, __source):
+        __position = []
+        for element in range(0, len(self.__Content)):
+            if self.__Content[element][1] == __source:
+                __position.append(element)
+        return __position
+
+    def removecontent(self, __source):
+        __position = self.sourceinlist(__source)
+        if not __position:
+            pass
+        else:
+            for element in range(0, len(__position)):
+                del self.__Content[int(__position[-1])]
+                del __position[-1]
 
 
 DB = Database()
